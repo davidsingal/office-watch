2e495c9e25cf2f9abf914fd1d69a1654ba323801
David Inga <david.inga@icloud.com>
2015-06-25 18:51:37 +0200

    sever and cron

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 'use strict';
 
 require('./app');
+
+var express = require('express')
+var server = express();
+
+server.set('port', (process.env.PORT || 5000))
+
+server.listen(server.get('port'), function() {
+  console.log("Node server is running at localhost:" + server.get('port'))
+})
