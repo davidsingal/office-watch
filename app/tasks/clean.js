5a45c9d97936e4b8bd76b0d35a2e9d601d81de9a
David Inga <david.inga@icloud.com>
2015-06-29 11:35:57 +0200

    clean task

--- app/tasks/clean.js
+++ app/tasks/clean.js
@@ -72,7 +72,7 @@ var callback = function(response) {
       var mailTemplate = handlebars.compile(tpl);
 
       // All mondays at 09:00 00 09 * * 1
-      schedule.scheduleJob('15 11 * * 1', function() {
+      schedule.scheduleJob('37 11 * * 1', function() {
         var message = { html: mailTemplate({ team: currentTurn }) };
         mailer('Cleaning time', message, recipients);
       });
