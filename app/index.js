be51ea8c87071698e93e3f636b5713a9a4de95ea
David Inga <david.inga@vizzuality.com>
2015-09-30 10:41:14 +0200

    added birthdays

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
 'use strict';
 
 require('./tasks/clean');
+require('./tasks/birthdays');
--- app/tasks/birthdays.js
+++ app/tasks/birthdays.js
@@ -27,9 +27,8 @@ function groupBy(array, f) {
 
 // Getting team and groups: name, email and _group is required
 var http = require('http');
-var query = 'SELECT cartodb_id AS id, name, email, _group' +
-  ' FROM ' + process.env.CARTODB_TABLENAME +
-  ' WHERE office=\'Madrid\' AND _group IS NOT NULL';
+var query = 'SELECT cartodb_id AS id, name, email, birthday' +
+  ' FROM ' + process.env.CARTODB_TABLENAME;
 var options = {
   host: process.env.CARTODB_USERNAME + '.cartodb.com',
   path: '/api/v2/sql?q=' + encodeURIComponent(query)
@@ -45,36 +44,31 @@ var callback = function(response) {
   //the whole response has been recieved, so we just print it out here
   response.on('end', function () {
     var team = JSON.parse(str).rows;
-    var groups = groupBy(team, function(g) {
-      return g._group;
-    });
-    var groupsLen = groups.length;
-    var turns = [];
-    var c = 0;
+    var today = moment();
+    var birthdays = [];
 
-    // Assign a gruop to each week of year
-    for (var w = weeksInYear; w--;) {
-      c = c + 1;
-      if (c === groupsLen) {
-        c = 0;
+    // Getting birthdays
+    _.each(team, function(t) {
+      var birth = moment(t.birthday);
+      if (birth.date() === today.date() &&
+        birth.month() === today.month()) {
+        t.age = today.year() - birth.year();
+        birthdays.push(t);
       }
-      turns.push(groups[c]);
-    }
+    });
 
-    // Turn of this week
-    var currentTurn = turns[currentWeek];
     var recipients = _.map(team, function(t) {
       return { email: t.email, name: t.name, type: 'to' };
     });
-    var tplPath = process.cwd() + '/app/templates/clean.handlebars';
+    var tplPath = process.cwd() + '/app/templates/birthdays.handlebars';
 
     fs.readFile(tplPath, 'utf8', function(err, tpl) {
       var mailTemplate = handlebars.compile(tpl);
 
       // All mondays at 09:00 00 09 * * 1
-      schedule.scheduleJob('00 09 * * 1', function() {
-        var message = { html: mailTemplate({ team: currentTurn }) };
-        mailer('Cleaning time', message, recipients);
+      schedule.scheduleJob('40 10 * * *', function() {
+        var message = { html: mailTemplate({ team: birthdays }) };
+        mailer('Happy birthday!', message, recipients);
         console.log('Mail sent!');
       });
     });
